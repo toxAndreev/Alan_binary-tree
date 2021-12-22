4b256410df712a983bfe9e401df31b0880d07599
Anton Andreev <neuner1995@mail.ru>
2021-12-22 15:28:26 +0300

    feat: add remove method

--- src/js/main.js
+++ src/js/main.js
@@ -34,4 +34,42 @@ class BinarySearchTree {
 		        }
 		    }
 		}
+
+		minNode(node) {
+	    if (node.left === null)
+	        return node;
+	    else
+	        return this.findMinNode(node.left);
+		}
+		remove(data) {
+		  this.root = this.removeNode(this.root, data);
+		}
+		removeNode(node, data) {
+	    if (node === null) {
+	        return null;
+	    } else if (data < node.data) {
+	        node.left = this.removeNode(node.left, data);
+	        return node;
+	    } else if (data > node.data) {
+	        node.right = this.removeNode(node.right, data);
+	        return node;
+	    } else {
+	        if (node.left === null && node.right === null) {
+	            node = null;
+	            return node;
+	        }
+	        if (node.left === null) {
+	            node = node.right;
+	            return node;
+	        } else if(node.right === null) {
+	            node = node.left;
+	            return node;
+	        }
+	        let newNode = this.minNode(node.right);
+	        node.data = newNode.data;
+	        node.right = this.removeNode(node.right, newNode.data);
+	        return node;
+	    }
+		}
 }
+
